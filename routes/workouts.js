6c7ad0191fc392f75d3bdca274871037d83d7df9
Tiffany Brand <brand2learn@gmail.com>
2020-10-16 11:38:07 -0400

    Comment workouts routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -4,33 +4,31 @@ const router = express.Router();
 
 const db = require('../models');
 
+// GET - Get all workouts
 router.get('/', async (req, res) => {
 	try {
 		const workouts = await db.Workout.find({});
-		console.log('in api workouts route');
 		res.json(workouts);
 	} catch (err) {
 		res.status(500).send(err);
 	}
 });
 
+// GET - Get all workouts
 router.get('/range', async (req, res) => {
 	try {
 		const workouts = await db.Workout.find({});
-		console.log('in api workouts route');
 		res.json(workouts);
 	} catch (err) {
 		res.status(500).send(err);
 	}
 });
 
+// POST - Create new workout with current date
 router.post('/', async (req, res) => {
-	console.log("in post route");
-	console.log(req.body);
 	try {
 		const workout = req.body;
 		workout.day = Date.now();
-		console.log(workout);
 		const result = await db.Workout.create(workout);
 		res.json(result);
 	} catch (err) {
@@ -38,12 +36,11 @@ router.post('/', async (req, res) => {
 	}
 });
 
+// PUT - Add exercises to a workout
 router.put('/:id', async (req, res) => {
 	console.log('in put route');
 	try {
 		const exercise = req.body;
-		console.log(exercise);
-		console.log(req.params.id);
 		const result = await db.Workout.findByIdAndUpdate(
 			req.params.id,
 			{ $push: { exercises: exercise } },
